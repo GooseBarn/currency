f9f23b30e8e7e155dc296b68d35dca75fc400094
Коротких А.А <evolenteam@mail.ru>
2020-04-20 01:25:11 +0300

    Тестовый сервис первый вариант

---
+++ src/main/java/testPackage/Currency.java
@@ -0,0 +1,45 @@
+package testPackage;
+
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+public class Currency {
+    private long id;
+    private String name; // в валюту
+    private List<Pair<String, Double>> value; // пара значений. Дата и стоимость
+
+    Currency(long id, String name) {
+        this.id = id;
+        this.name = name;
+        value = new ArrayList<Pair<String, Double>> ();
+        for (int i=0; i<10; i++){
+            value.add( new Pair<String, Double>("testDte", Math.random()*30));
+        }
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Pair<String, Double>> getValue() {
+        return value;
+    }
+
+    public void setId(long param1) {
+        this.id = param1;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+}
---
+++ src/main/java/testPackage/CurrencyController.java
@@ -0,0 +1,20 @@
+package testPackage;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CurrencyController {
+    private static final String template = "%s";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/currency")
+    @ResponseBody
+    public Currency sayHello(@RequestParam(name="name", required=false, defaultValue="USD") String name) {
+        return new Currency(counter.incrementAndGet(), String.format(template, name));
+    }
+}
