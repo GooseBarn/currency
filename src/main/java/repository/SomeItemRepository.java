c1c0b239b13525cd439c5d8489ed84c7c34bdab0
Коротких А.А <evolenteam@mail.ru>
2020-04-22 11:36:38 +0300

    fix запроса

--- src/main/java/repository/SomeItemRepository.java
+++ src/main/java/repository/SomeItemRepository.java
@@ -13,21 +13,22 @@ import java.util.List;
 
 public class SomeItemRepository {
 
-
     public List<Pair<String, Double>> getPriceCurrencyFromTo(String nameCurrency, String from, String to) throws SQLException, IOException {
         List<Pair<String, Double>> items = new ArrayList<Pair<String, Double>>();
         String query = "select p.* \n" +
                 "from public.\"price\" as p\n" +
-                "where p.date >= \'"+from+"\'::DATE \n" +
-                "\t\tand \n" +
-                "\tp.date <= \'"+to+"\'::DATE \n" +
-                "\tand p.currency = 1";
+                "where p.date >= \'" + from + "\'::DATE \n" +
+                "and \n" +
+                "p.date <= \'" + to + "\'::DATE \n" +
+                "and p.currency = (select c.id from currency c\n" +
+                "where c.name = '"+nameCurrency+"') \n" +
+                "order by p.date;";
         selectFromtable(items, query);
         return items;
     }
 
 
-   private void selectFromtable(List<Pair<String, Double>> items, String query) throws SQLException, IOException {
+    private void selectFromtable(List<Pair<String, Double>> items, String query) throws SQLException, IOException {
         Connection connection = DBConnector.getConnection();
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery(query);
